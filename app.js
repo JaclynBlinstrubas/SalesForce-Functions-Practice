d6d0dea2cbb8627f60b82c942d3984e053b33999
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2022-02-21 15:58:46 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+//declare function
+function calculateGearRatio(driverGear, drivenGear) {
+  return driverGear / drivenGear
+}
+
+//call function
+let gearRatio = calculateGearRatio(42, 30)
+console.log(gearRatio) // 1.4
