db25aa07d550d76d7ec0fd2315ce11f88ad9e86f
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2022-02-21 16:15:39 -0500

    function expressions

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-//declare function
-function calculateGearRatio(driverGear, drivenGear) {
-  return driverGear / drivenGear
-}
+//// FUNCTION DECLARATION ////
 
-//call function
-let gearRatio = calculateGearRatio(42, 30)
-//function is declared after the line it is called
-// this is allowed in function declaration
-function calculateGearRatio(driverGear, drivenGear) {
+// // declare function
+// function calculateGearRatio(driverGear, drivenGear) {
+//   return driverGear / drivenGear
+// }
+// // call function
+// let gearRatio = calculateGearRatio(42, 30)
+// console.log(gearRatio) // 1.4
+
+// --------------
+
+// // call function
+// let gearRatio = calculateGearRatio(42, 30);
+// // function is declared after the line it is called
+// // this is allowed in function declaration
+// function calculateGearRatio(driverGear, drivenGear){
+//   return (driverGear / drivenGear);
+// }
+// console.log(gearRatio); // 1.4
+
+// --------------
+
+//// FUNCTION EXPRESSIONS ////
+
+const calculateGearRatio = function (driverGear, drivenGear) {
   return driverGear / drivenGear
 }
+// the rest works the same
+let gearRatio = calculateGearRatio(42, 30)
 console.log(gearRatio) // 1.4
