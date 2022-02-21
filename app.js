92ed4533bbd3cd440700887d8ccf2aa04c16ad1e
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2022-02-21 16:29:52 -0500

    anonymous functions

--- app.js
+++ app.js
@@ -34,12 +34,22 @@
 
 //// RETURNING A FUNCTION  ////
 
-// when invoked, this function will assign a function
-function gearFactory() {
-  return function (driverGear, drivenGear) {
-    return driverGear / drivenGear
-  }
-}
-// calculateGearRatio can now be invoked as a function
-const calculateGearRatio = gearFactory()
-// and all the rest
+// // when invoked, this function will assign a function
+// function gearFactory() {
+//   return function (driverGear, drivenGear) {
+//     return driverGear / drivenGear
+//   }
+// }
+// // calculateGearRatio can now be invoked as a function
+// const calculateGearRatio = gearFactory()
+// // and all the rest
+
+// --------------
+
+//// ANONYMOUS FUNCTIONS ////
+
+let myArray = [1, 5, 11, 17]
+let newArray = myArray.map(function (item) {
+  return item / 2
+})
+console.log(newArray) // [0.5, 2.5, 5.5, 8.5]
