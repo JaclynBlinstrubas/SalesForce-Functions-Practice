be761921498dcb039592eb0fbf4e1113c673315a
JBlin1819 <jaclyn.blinstrubas@gmail.com>
2022-02-21 16:06:20 -0500

    call function before declared example

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ function calculateGearRatio(driverGear, drivenGear) {
 
 //call function
 let gearRatio = calculateGearRatio(42, 30)
+//function is declared after the line it is called
+// this is allowed in function declaration
+function calculateGearRatio(driverGear, drivenGear) {
+  return driverGear / drivenGear
+}
 console.log(gearRatio) // 1.4
